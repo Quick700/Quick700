ebd015fc2ed37fff307577cb40fb272646087955
Calvin Beck <hobbes@ualberta.ca>
2018-10-24 13:00:13 -0400

    Udpated configuration script and readme.

--- honggfuzz/make-configs.sh
+++ honggfuzz/make-configs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed "s|\$HOME|$HOME|g" httpd-template.conf.h1 > httpd.conf.h1
-sed "s|\$HOME|$HOME|g" httpd-template.conf.h2 > httpd.conf.h2
+sed "s|\$HOME/fuzzpache|$PWD/dist|g" httpd-template.conf.h1 > httpd.conf.h1
+sed "s|\$HOME/fuzzpache|$PWD/dist|g" httpd-template.conf.h2 > httpd.conf.h2
