1ff641c21b13c919192e597403664e093f9ad4d4
Yishuai Li <yishuai@cis.upenn.edu>
2018-12-22 00:42:04 -0600

    honggfuzz: fix Docker

--- honggfuzz/compile.sh
+++ honggfuzz/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd apache
+patch -p1 < ../hong-apache/httpd-master.honggfuzz.patch
 ../compile_and_install.asan.sh
 cd -
 ./make-configs.sh
--- honggfuzz/fuzz-h1.sh
+++ honggfuzz/fuzz-h1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-honggfuzz/honggfuzz -f hong-apache/corpus_http1 -w hong-apache/httpd.wordlist -- ./dist/bin/httpd -DFOREGROUND -f /Users/yishuai/quick700/honggfuzz/httpd.conf.h1 ___FILE___
+honggfuzz/honggfuzz -f hong-apache/corpus_http1 -w hong-apache/httpd.wordlist -- ./dist/bin/httpd -DFOREGROUND -f /home/honggfuzz/httpd.conf.h1 ___FILE___
